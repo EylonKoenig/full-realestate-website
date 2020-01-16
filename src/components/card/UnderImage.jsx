e5ae32b0fb5cf7377d68060fc2a74b9fd9fca123
EylonKoenig <eylonkoenigm@walla.com>
2020-01-16 11:21:23 +0200

    country and cities filter

--- src/components/card/UnderImage.jsx
+++ src/components/card/UnderImage.jsx
@@ -8,30 +8,29 @@ class UnderImage extends React.Component {
         return (
             <div>
                 {this.props.cardType === 'loading' ?
-                <div className={'underpicuercontent'}>
-                    <div style={{width:'80%',margin:'auto',paddingBottom:'1px'}}>
-                        <div className={'loading'} style={{width:`50%`}}>
-                        </div>
-                        <div className={'loading'}>
+                    <div className={'underpicuercontent'}>
+                        <div style={{ width: '80%', margin: 'auto', paddingBottom: '1px' }}>
+                            <div className={'loading'} style={{ width: `50%` }}>
+                            </div>
+                            <div className={'loading'}>
+                            </div>
                         </div>
                     </div>
-                </div>
                     : apartment.cardType === 'apartment' &&
-                            <div className={'underpicuercontent'}>
-                    <ul className={'pictuerdetails'}>
-                        <li>bed :<b>{apartment.number_of_bath}</b></li>
-                        <li>bath :<b>{apartment.number_of_room}</b></li>
+                    <div className={'underpicuercontent'}>
+                        <ul className={'pictuerdetails'}>
+                            <li>room :<b>{apartment.number_of_room}</b></li>
+                            <li>bath :<b>{apartment.number_of_bath}</b></li>
+                        </ul>
 
-                    </ul>
-
-                    <div className={'underpicuercontentLast'}>
-                        Country: {apartment.country} {`  City: ${apartment.city_name}`}
-                    </div>
+                        <div className={'underpicuercontentLast'}>
+                            Country: {apartment.country} {`  City: ${apartment.city_name}`}
+                        </div>
                         <div className={'email-agent'}>
                             {/*need to add email agent action as  obj.email_agent*/}
                             <button>Email Agent</button>
                         </div>
-                </div>
+                    </div>
                 }
             </div>
 
